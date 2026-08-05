c25e3b71a47b156aeab8f186e2f88081e7c0dc33
Stakkino <jhenstakkino@gmail.com>
2026-08-05 16:58:03 +0300

    mise a jour des models

--- backend/utilisateurs/models.py
+++ backend/utilisateurs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Utilisateur(AbstractUser):
+    ROLE = [
+        ('admin', 'Administrateur'),
+        ('client', 'Client'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE, default='client')
+    telephone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
